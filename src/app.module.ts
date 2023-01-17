03d2fcb06f1a0945ba204cc5b936498a9cc48861
Martin Felipe Espinal Cruces <cofy43@gmail.com>
2023-01-17 15:41:27 -0600

    import google api and env packages

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { ApiModule } from './api/api.module';
+import { getEnvPath } from './common/helper/env.helper';
+
+const envFilePath: string = getEnvPath(`${__dirname}/common/envs`);
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({ envFilePath, isGlobal: true }), ApiModule],
   controllers: [AppController],
   providers: [AppService],
 })
